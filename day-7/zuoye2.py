0a07b63bf8235d97e6f61b0937237967f2f725ff
周涵 <997703999@qq.com>
2020-03-13 23:38:06 +0800

    .

---
+++ day-7/zuoye2.py
@@ -0,0 +1,29 @@
+fruit = 'banana'
+letter = fruit[1]
+print(letter)
+
+
+
+
+
+word = 'banana'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+print(count)
+
+
+word = 'banana'
+new_word = word.upper()
+print(new_word)
+
+
+word = 'banana'
+index = word.find('a')
+print(index)
+
+
+word = 'banana'
+cnt = word.count('a')
+print(cnt)
